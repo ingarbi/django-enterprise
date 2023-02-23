6488b045bb5426860f41973b4953d65b68c8bef2
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-02-23 12:10:30 +0300

    models exercises chapter_3, loadded fixtures

--- chapter_4/urls.py
+++ chapter_4/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, register_converter
 from django.views.generic import TemplateView, RedirectView
 from .converters import YearConverter
+from .views import practice_view, practice_year_view, vehicle_view
 
 register_converter(YearConverter, 'year')
 
@@ -11,8 +12,7 @@ urlpatterns = [
     path('my_path/my_unwanted_url/', RedirectView.as_view(
         url='http://localhost:8000/my_wanted_url/', permanent=True)
     ),
-    path('my_path/<year:year>/', TemplateView.as_view(
-        template_name='chapter_4/index.html')
-    ),
-
+    # path('my_year_path/<year:year>/', practice_view),
+    path('my_year_path/<year:year>/', practice_year_view, name='year_url'),
+    path('vehicle/<int:id>/', vehicle_view, name='vehicle-detail'),
 ]
--- chapter_4/views.py
+++ chapter_4/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.template.response import (
+    TemplateResponse
+)
+from chapter_3.models import Vehicle
+from django.http import Http404
 
-# Create your views here.
+
+def practice_view(request, year):
+    return TemplateResponse(
+        request, 'chapter_4/my_practice_page.html',
+        {'year': year}
+    )
+
+
+def practice_year_view(request, year):
+    if year >= 1900:
+        return TemplateResponse(
+            request,
+            'chapter_4/my_year.html',
+            {'year': year}
+        )
+    else:
+        raise Http404(f'Year Not Found: {year}')
+
+
+def vehicle_view(request, id):
+    try:
+        vehicle = Vehicle.objects.get(id=id)
+    except Vehicle.DoesNotExist:
+        raise Http404(f'Vehicle ID Not Found: {id}')
+    return TemplateResponse(
+        request,
+        'chapter_4/my_vehicle.html',
+        {'vehicle': vehicle}
+    )
